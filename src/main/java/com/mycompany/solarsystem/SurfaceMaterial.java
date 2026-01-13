7913083638b7d8ea5b27d753e87d1edee415ce1f
kandy <kandy45159kochar@gmail.com>
2026-01-13 10:00:00 +0000

    Add SurfaceMaterial enum for planet composition types

---
+++ src/main/java/com/mycompany/solarsystem/SurfaceMaterial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assignment04.planets;
+
+/**
+ *
+ * @author Kandy kochar
+ */
+// Enum representing possible surface materials
+public enum SurfaceMaterial {
+    TERRESTRIAL,
+    GASEOUS,
+    ICY
+}
